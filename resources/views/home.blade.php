b4262eeb64b5eeaf23a3d88f75dc11938c62447e
Soumik-sarker01 <soumiksarkerofficial@gmail.com>
2022-04-14 01:17:46 +0600

    Added Tourist dashboard frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,59 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Tourist Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dashboard.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <style>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    </style>
+
+  <body>
+<html>
+
+   <head>
+      <title>Home</title>
+   </head>
+
+   <body onload="myFunction()" style="margin:0">
+     <div class="wrapper" style="min-height: 100vh">
+
+       <div>
+         <h4 class="subtitle">city</h4><hr class="divider">
+       </div>
+
+      <div id="loader"></div>
+
+       <div class="feature-package  animate-bottom" id="myDiv" style="display: none">
+        <div class="card">
+                  <a href="#" style="text-decoration: none">
+                 <div class="card-image"><img src="images/dhaka.jpg" height="120" width="240"></div>
+                 <div class="container">
+                   <h5><b>Dhaka</b></h5>
+                 </div></a>
+                 </div>
+       </div>
+     </div>
+
+
+      <script>
+        var myVar;
+        function myFunction() {
+          myVar = setTimeout(showPage, 500);
+        }
+          function showPage() {
+          document.getElementById("loader").style.display = "none";
+          document.getElementById("myDiv").style.display = "flex";
+        }
+      </script>
+   </body>
+</html>
 @endsection
